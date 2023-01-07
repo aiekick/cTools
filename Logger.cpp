c4ac9a4b22d4ed5dc665104b9abdcbcab8b1ea85
aiekick <aiekick@funparadigm.com>
2023-01-07 04:24:21 +0100

    [ADD] : add cote for bitwize common operations

--- Logger.cpp
+++ Logger.cpp
@@ -30,7 +30,7 @@ SOFTWARE.
 #ifdef USE_GLFW3
 #include <GLFW/glfw3.h>
 #elif defined(USE_SDL2)
-#include <SDL/include/SDL.h>
+#include <SDL.h>
 #endif
 #if defined(TRACY_ENABLE) && defined(LOG_TRACY_MESSAGES)
 #include <tracy/Tracy.hpp>
--- cTools.cpp
+++ cTools.cpp
@@ -90,6 +90,34 @@ SOFTWARE.
 }
 #endif
 
+/////////////////////////////////////////////////////////////
+///////// bitwize ///////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+bool ct::isbitset(int32_t vContainer, int32_t vBit)
+{
+	return ((vContainer & vBit) != 0);
+}
+
+bool ct::isbitset_exclusive(int32_t vContainer, int32_t vBit)
+{
+	return ((vContainer & vBit) != vBit);
+}
+
+void ct::setbit(int32_t vContainer, int32_t vBit)
+{
+	vContainer |= vBit;
+}
+
+void ct::unsetbit(int32_t vContainer, int32_t vBit)
+{
+	vContainer = vContainer & ~vBit;
+}
+
+/////////////////////////////////////////////////////////////
+///////// splitStringToVector ///////////////////////////////
+/////////////////////////////////////////////////////////////
+
 ::std::list<::std::string> ct::splitStringToList(const ::std::string& text, const std::string& delimiters, bool pushEmpty, bool vInversion)
 {
 	::std::list<::std::string> arr;
